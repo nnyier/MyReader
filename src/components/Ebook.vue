550e25e7cdd269bbac5e4d20a9f66f81e1db5e29
nnyier <1049194608@qq.com>
2018-12-26 15:22:39 +0800

    finish

--- src/components/Ebook.vue
+++ src/components/Ebook.vue
@@ -16,6 +16,13 @@
               :ifTitleAndMenuShow="ifTitleAndMenuShow"
               :fontSizeList="fontSizeList"
               :defaultFontSize="defaultFontSize"
+              :themeList="themeList"
+              :defaultTheme="defaultTheme"
+              :bookAvailable="bookAvailable"
+              :navigation="navigation"
+              @jumpTo="jumpTo"
+              @onProgressChange="onProgressChange"
+              @setTheme="setTheme"
               @setFontSize="setFontSize"></menu-bar>
   </div>
 </template>
@@ -24,7 +31,8 @@
 import TitleBar from "../views/TitleBar";
 import MenuBar from "../views/MenuBar";
 import Epub from "epubjs";
-const DOWNLOAD_URL = "/2018_Book_AgileProcessesInSoftwareEngine.epub";
+// const DOWNLOAD_URL = "/2018_Book_AgileProcessesInSoftwareEngine.epub";
+const DOWNLOAD_URL = "/1365.epub";
 
 export default {
   name: "Ebook",
@@ -58,11 +66,54 @@ export default {
           fontSize: 24
         }
       ],
-      defaultFontSize: 14
+      defaultFontSize: 14,
+      themeList: [
+        {
+          name: "default",
+          style: {
+            body: {
+              color: "#000",
+              background: "#fff"
+            }
+          }
+        },
+        {
+          name: "eye",
+          style: {
+            body: {
+              color: "#000",
+              background: "#cee"
+            }
+          }
+        },
+        {
+          name: "night",
+          style: {
+            body: {
+              color: "#fff",
+              background: "#000"
+            }
+          }
+        },
+        {
+          name: "glod",
+          style: {
+            body: {
+              color: "#000",
+              background: "rgb(241,236,226)"
+            }
+          }
+        }
+      ],
+      defaultTheme: 0,
+      // 图书可用状态
+      bookAvailable: false,
+      navigation: {}
     };
   },
   mounted() {
     this.showEpub();
+    this.setTheme(this.defaultTheme);
   },
   methods: {
     showEpub() {
@@ -80,6 +131,55 @@ export default {
       this.themes = this.rendition.themes;
       // 设置默认字体
       this.setFontSize(this.defaultFontSize);
+      // 主题切换
+      // this.themes.register(name.styles) 注册
+      // this.themes.select(name) 选择
+      this.registerTheme();
+      // this.themes.select("night");
+      // 获取location对象
+      // console.log(this.book.locations);
+      // 默认不会生成
+      this.book.ready
+        .then(() => {
+          // 目录
+          this.navigation = this.book.navigation;
+          return this.book.locations.generate();
+        })
+        .then(result => {
+          this.locations = this.book.locations;
+          // this.onProgressChange(1);
+          // 生成后将状态设置为true
+          this.bookAvailable = true;
+        });
+    },
+    // 根据链接跳转目录
+    jumpTo(href) {
+      this.rendition.display(href);
+      this.hideTitleAndMenu();
+    },
+    hideTitleAndMenu() {
+      // 隐藏标题栏和菜单栏
+      this.ifTitleAndMenuShow = false;
+      // 隐藏菜单栏弹出的设置栏
+      this.$refs.MenuBar.hideSetting();
+      // 隐藏目录
+      this.$refs.MenuBar.hideContent();
+    },
+    registerTheme() {
+      this.themeList.forEach(theme => {
+        this.themes.register(theme.name, theme.style);
+      });
+    },
+    setTheme(index) {
+      this.themes.select(this.themeList[index].name);
+      this.defaultTheme = index;
+    },
+    // progress 进度条数值（0-100）
+    onProgressChange(progress) {
+      const percentage = progress / 100;
+      const location =
+        percentage > 0 ? this.locations.cfiFromPercentage(percentage) : 0;
+      this.rendition.display(location);
     },
     prevPage() {
       if (this.rendition) {
--- src/views/MenuBar.vue
+++ src/views/MenuBar.vue
@@ -5,24 +5,28 @@
            :class="{'hide-box-shadow': ifSettingShow || !ifTitleAndMenuShow}"
            v-show="ifTitleAndMenuShow">
         <div class="icon-wrapper">
-          <div class="icon-menu icon"></div>
+          <div class="icon-menu icon"
+               @click="showSetting(3)"></div>
         </div>
         <div class="icon-wrapper">
-          <div class="icon-progress icon"></div>
+          <div class="icon-progress icon"
+               @click="showSetting(2)"></div>
         </div>
         <div class="icon-wrapper">
-          <div class="icon-bright icon"></div>
+          <div class="icon-bright icon"
+               @click="showSetting(1)"></div>
         </div>
         <div class="icon-wrapper">
           <div class="icon-a icon"
-               @click="showSetting">A</div>
+               @click="showSetting(0)">A</div>
         </div>
       </div>
     </transition>
     <transition name="slide-up">
       <div class="setting-wrapper"
            v-show="ifSettingShow">
-        <div class="setting-font-size">
+        <div class="setting-font-size"
+             v-if="showTag === 0">
           <!-- 从字体大小数组中 取最大字体和最小字体 -->
           <div class="preview"
                :style="{fontSize:fontSizeList[0].fontSize+'px'}">A</div>
@@ -41,10 +45,49 @@
               <div class="line"></div>
             </div>
           </div>
-
           <div class="preview"
                :style="{fontSize:fontSizeList[fontSizeList.length-1].fontSize+'px'}">A</div>
         </div>
+        <div class="setting-theme"
+             v-else-if="showTag === 1">
+          <div class="setting-theme-item"
+               v-for="(item,index) in themeList"
+               :key="index">
+            <div class="preview"
+                 @click.stop="setTheme(index)"
+                 :style="{background: item.style.body.background}"
+                 :class="{'no-border':item.style.body.background !== '#fff'}"></div>
+            <div class="text"
+                 :class="{'select': index === defaultTheme}">{{item.name}}</div>
+          </div>
+        </div>
+        <div class="setting-progress"
+             v-else-if="showTag === 2">
+          <div class="progress-wrapper">
+            <input class="progress"
+                   type="range"
+                   max="100"
+                   min="0"
+                   step="1"
+                   @change="onProgressChange($event.target.value)"
+                   @input="onProgressInput(($event.target.value))"
+                   :value="progress"
+                   :disabled="!bookAvailable"
+                   ref="progress">
+          </div>
+          <div class="text-wrapper">{{bookAvailable?progress+'%':'加载中...'}}</div>
+        </div>
+      </div>
+    </transition>
+    <content-view :ifShowContent="ifShowContent"
+                  v-show="ifShowContent"
+                  :navigation="navigation"
+                  :bookAvailable="bookAvailable"
+                  @jumpTo="jumpTo"></content-view>
+    <transition name="fade">
+      <div class="content-mask"
+           v-show="ifShowContent"
+           @click="hideContent">
       </div>
     </transition>
   </div>
@@ -52,12 +95,18 @@
 
 <script>
 // @ is an alias to /src
-
+import ContentView from "./ContentView";
 export default {
-  name: "home",
+  name: "MenuBar",
+  components: {
+    ContentView
+  },
   data() {
     return {
-      ifSettingShow: false
+      ifSettingShow: false,
+      showTag: 0,
+      progress: 0,
+      ifShowContent: false
     };
   },
   props: {
@@ -66,18 +115,46 @@ export default {
       default: false
     },
     fontSizeList: Array,
-    defaultFontSize: Number
+    defaultFontSize: Number,
+    themeList: Array,
+    defaultTheme: Number,
+    bookAvailable: Boolean,
+    navigation: Object
   },
   methods: {
     setFontSize(fontSize) {
       // 向父组件传递事件
       this.$emit("setFontSize", fontSize);
     },
-    showSetting() {
-      this.ifSettingShow = true;
+    setTheme(defaultTheme) {
+      this.$emit("setTheme", defaultTheme);
+    },
+    showSetting(tag) {
+      this.showTag = tag;
+      if (this.showTag === 3) {
+        this.ifSettingShow = false;
+        this.ifShowContent = true;
+      } else {
+        this.ifSettingShow = true;
+      }
     },
     hideSetting() {
       this.ifSettingShow = false;
+    },
+    // 松手事件
+    onProgressChange(progress) {
+      this.$emit("onProgressChange", progress);
+    },
+    // 拖动事件 动态设置进度条背景
+    onProgressInput(progress) {
+      this.progress = progress;
+      this.$refs.progress.style.backgroundSize = `${this.progress}% 100%`;
+    },
+    hideContent() {
+      this.ifShowContent = false;
+    },
+    jumpTo(target) {
+      this.$emit("jumpTo", target);
     }
   },
   watch: {
@@ -184,6 +261,86 @@ export default {
         }
       }
     }
+    .setting-theme {
+      height: 100%;
+      display: flex;
+      .setting-theme-item {
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        padding: px2rem(5);
+        box-sizing: border-box;
+        .preview {
+          flex: 1;
+          border: px2rem(1) solid #ccc;
+          box-sizing: border-box;
+          &.no-border {
+            border: none;
+          }
+        }
+        .text {
+          flex: 0 0 px2rem(30);
+          font-size: px2rem(16);
+          color: #ccc;
+          @include center;
+          &.select {
+            color: #333;
+          }
+        }
+      }
+    }
+    .setting-progress {
+      position: relative;
+      width: 100%;
+      height: 100%;
+      .progress-wrapper {
+        width: 100%;
+        height: 100%;
+        padding: 0 px2rem(30);
+        box-sizing: border-box;
+        @include center;
+        .progress {
+          width: 100%;
+          -webkit-appearance: none;
+          height: px2rem(2);
+          background: -webkit-linear-gradient(#999, #999) no-repeat, #ddd;
+          background-size: 0 100%;
+
+          &:focus {
+            outline: none;
+          }
+          // 小手柄
+          &::-webkit-alider-thumb {
+            -webkit-appearance: none;
+            height: px2rem(20);
+            width: px2rem(20);
+            border-radius: 50%;
+            background: #fff;
+            box-shadow: 0 px2rem(4) px2rem(4) rgba(0, 0, 0, 0.15);
+            border: px2rem(1) solid #ddd;
+          }
+        }
+      }
+      .text-wrapper {
+        position: absolute;
+        left: 0;
+        bottom: 0;
+        width: 100%;
+        text-align: center;
+        font-size: px2rem(12);
+        color: #333;
+      }
+    }
+  }
+  .content-mask {
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 101;
+    display: flex;
+    width: 100%;
+    height: 100%;
+    background: rgba(51, 51, 51, 0.8);
   }
 }
 </style>
